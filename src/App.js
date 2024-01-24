92b585853f35ea3c059ccf15dc3c3aa19058140c
IrfanCatovic <catovicc84@gmail.com>
2024-01-24 17:34:54 +0100

    fetch question for our quiz

--- src/App.js
+++ src/App.js
@@ -2,14 +2,43 @@
 import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import { useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading", //handling statuses of aplication
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action uknown");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch("http://localhost:8000/questions") //run it on mount
       .then((res) => res.json()) //we get response we need to convert in json
-      .then((data) => console.log(data)) //will return another promise and we need to chain another then
+      .then((data) => dispatch({ type: "dataReceived", payload: data })) //will return another promise and we need to chain another then
       //and this gives us another data and we use that data
-      .catch((err) => console.error("Error")); //we catch errors
+      .catch((err) => dispatch({ type: "dataFailed" })); //we catch errors
   });
 
   return (
