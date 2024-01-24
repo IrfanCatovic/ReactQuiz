1588710456e6e33feba3f10517b24bf935af0dd0
IrfanCatovic <catovicc84@gmail.com>
2024-01-24 15:56:24 +0100

    Main added comp

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import DateCounter from "./DateCounter";
 import Header from "./Header";
+import Main from "./Main";
 
 export default function App() {
   return (
     <div className="app">
       {/* <DateCounter /> */}
       <Header />
+
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
