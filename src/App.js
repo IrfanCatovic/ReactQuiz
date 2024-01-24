f50b6185f5ddb69178cc6fbd7a26b69694f73dd5
IrfanCatovic <catovicc84@gmail.com>
2024-01-24 16:13:51 +0100

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 // import DateCounter from "./DateCounter";
+import { useEffect } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
 export default function App() {
+  useEffect(function () {
+    fetch("http://localhost:8000/questions") //run it on mount
+      .then((res) => res.json()) //we get response we need to convert in json
+      .then((data) => console.log(data)) //will return another promise and we need to chain another then
+      //and this gives us another data and we use that data
+      .catch((err) => console.error("Error")); //we catch errors
+  });
+
   return (
     <div className="app">
       {/* <DateCounter /> */}
